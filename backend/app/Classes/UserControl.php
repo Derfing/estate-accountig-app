0ce6c4385daea3639c3337bf03f193d99f4307da
Derfing <derendaevkosta45@gmail.com>
2023-04-09 14:45:56 +0300

    added error handler to editProfile

--- backend/app/Classes/UserControl.php
+++ backend/app/Classes/UserControl.php
@@ -5,6 +5,7 @@ namespace App\Classes;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Human;
+use Symfony\Component\HttpFoundation\RequestMatcher\PathRequestMatcher;
 
 class UserControl
 {
@@ -15,21 +16,26 @@ class UserControl
         $patronymic = $request['patronymic'];
         $speciality = $request['speciality'];
 
+        if (!$firstname || !$lastname || !$patronymic || !$speciality) {
+            return response(['status' => 'Ошибка: Недостаточно введены данные запроса']);
+        }
+
         $user = User::find($login);
         $human = Human::find($user->human_id);
+
+        if (!$user || !$human) {
+            return response(['status' => 'Ошибка: Не существует пользователя с таким логином или он неправильно заполнен.']);
+        }
 
         $user->speciality = $speciality;
         $human->surname = $lastname;
         $human->first_name = $firstname;
         $human->patronymic = $patronymic;
 
-        $user->save();
-        $human->save();
-
         if ($user->save() &&  $human->save()) {
-            return ['status', '0'];
+            return ['status' => 'ok'];
         } else {
-            return ['status', '-1'];
+            return ['status' => 'Ошибка: Ошибка при сохранении пользователя.'];
         }
     }
 
